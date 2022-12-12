bb363421be8d29ac5cae98ae367e7a32da244da3
Alex Simon <alexanderleesimon@gmail.com>
2022-12-12 13:12:11 -0500

    reworked about, nav, and projects

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,26 +3,26 @@ import myPicture from './79182621.jpeg';
 
 function About() {
   return (
-    <div className="about-me">
+    <div className="about-me" >
       <div className="greet-and-picture">
         <h1 className="hello">Hi, I'm Alex.</h1>
-        <h1 className='my-picture'>
+        <p className='my-picture'>
           <img src={myPicture} alt="portrait"/>
-        </h1>
+        </p>
       </div>
-      <div className="about-and-buttons">
+      <div className="about-and-video" id="about-me">
         <h2>I am a self-taught programmer and life-long tech enthusiast, who has transitioned from a career as a civil litigation attorney.</h2>
-        <ul className="quick-nav">
-          <li>
-            <a href="#contact">Contact Me</a>
-          </li>
-          <li>
-            <a href="#projects">See My Past Projects</a>
-          </li>
-        </ul>
       </div>
       <div className="intro-video">
-        <p>[Insert Intro Video]</p>
+        <video
+          src=""
+          poster=""
+          width="854"
+          height="480"
+          preload="auto"
+          controls>
+          <p>Introduction of Myself</p>
+        </video>
       </div>
     </div>
   );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,42 +1,32 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import experience from "../data";
-import Stack from "react-bootstrap/Stack";
 
 function Projects() {
+  const projects = experience.projects;
+  const projectsList = projects.map((project) => (
+    <div className="project-list">
+      <h3 className="project-title">{project.title} | {project.reason}</h3>
+      <p className="project-desc">{project.description}</p>
+      <div className={`${project.title}-video`}>
+        <video
+          src=""
+          poster=""
+          width="854"
+          height="480"
+          preload="auto"
+          controls>
+          <p>{`${project.title} Video`}</p>
+        </video>
+      </div>
+      <p className="project-details">{project.tech} |  <a href={project.link} className={`${project.title}-github`}>Github</a></p>
+    </div>
+  ));
+
   return(
-    <Container id="projects">
-      <Row>
-        <Col style={{fontSize: 100}}>
-          My Projects
-        </Col>
-      </Row>
-      {experience.projects.map((project) => (
-        <Container>
-          <Row style={{fontSize: 50}}>
-            <Stack direction="horizontal" gap={3}>
-              <div>{project.title}</div>
-              <div className="vr"/>
-              <div>{project.reason}</div>
-            </Stack>
-          </Row>
-          <Row>
-            <Stack direction="horizontal" gap={3}>
-              <div>{project.description}</div>
-              <div>{project.video}</div>
-            </Stack>
-          </Row>
-          <Row>
-            <Stack direction="horizontal" gap={3}>
-              <div>{project.tech}</div>
-              <div><a href={project.link}>GitHub Repo</a></div>
-            </Stack>
-          </Row>
-        </Container>
-      ))}
-    </Container>
+    <div className="projects" id="projects">
+      <h1 className="projects-title">My Projects</h1>
+      <p>{projectsList}</p>
+    </div>
   );
 }
 
