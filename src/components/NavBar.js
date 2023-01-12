027601e3c98002ce658ed714898a7deb042d09b9
Alex Simon <alexanderleesimon@gmail.com>
2023-01-12 16:37:40 -0500

    added hamburger, drop down menu, and listener

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
+import NavHamburger from "./NavHamburger";
 function NavBar() {
+  const [navHamburgerOpen, setHamburgerOpen] = useState(false);
+
+  const toggleHamburger = () => {
+    setHamburgerOpen(!navHamburgerOpen);
+  }
+
+  const handleResize = () => {
+    if(navHamburgerOpen) {
+      toggleHamburger();
+    }
+  }
+
+  window.addEventListener('resize', handleResize);
+  window.addEventListener('navigate', handleResize);
+
   return(
     <nav className="nav-bar">
       <p className="nav-bar-title">
           <a href="#top" className="nav-bar-title-link">Alex Simon</a>
       </p>
       <ul className="nav-bar-buttons">
+        <li>
+          <div onClick={toggleHamburger}>
+            <NavHamburger />
+          </div>
+        </li>
         <li>
           <a href="#about-me" className="nav-bar-link">About</a>
         </li>
@@ -20,6 +41,23 @@ function NavBar() {
         </li>
         <li>
           <a href="#contact" className="nav-bar-link">Contact</a>
+        </li>
+      </ul>
+      <ul className="nav-bar-buttons-vert" style={{display: (navHamburgerOpen) ? "flex": "none"}}>
+        <li>
+          <a href="#about-me" className="nav-bar-link-vert">About</a>
+        </li>
+        <li>
+          <a href="#projects" className="nav-bar-link-vert">Projects</a>
+        </li>
+        <li>
+          <a href="#skills" className="nav-bar-link-vert">Skills</a>
+        </li>
+        <li>
+          <a href="#education" className="nav-bar-link-vert">Education</a>
+        </li>
+        <li>
+          <a href="#contact" className="nav-bar-link-vert">Contact</a>
         </li>
       </ul>
     </nav>
